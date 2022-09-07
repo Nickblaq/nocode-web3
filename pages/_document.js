40c3ca8e07a1a00f3130d42dde0396774d5f8501
nickblaq <nickookoro200@gmail.com>
2022-09-07 12:43:46 +0100

    0.5.2

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,7 @@ class MyDocument extends Document {
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
             {/* <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&family=Ramaraja&display=swap" rel="stylesheet" /> */}
     </Head>
-        <body className="bg-gray-900 text-white">
+        <body className="bg-gray-900 text-white min-w-full">
           <Main />
           <NextScript />
         </body>
