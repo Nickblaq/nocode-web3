7e7dc00fa43e71bdcd0188c7c443d7ae7e8ca94c
nickblaq <nickookoro200@gmail.com>
2022-09-15 11:40:22 +0100

    0.6.7.0

--- pages/_document.js
+++ pages/_document.js
@@ -6,8 +6,8 @@ class MyDocument extends Document {
     return (
       <Html className="dark" lang="en">
          <Head>
-            <meta name="description" content="Pissar nocode token generator" />
-            <link rel="icon" href="/verified.svg" />
+            <meta name="description" content="Bep20 nocode token generator" />
+            <link rel="icon" href="/favicon.ico" />
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
             {/* <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&family=Ramaraja&display=swap" rel="stylesheet" /> */}
